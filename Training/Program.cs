9bf88d7fdc9719384a6a60f8065e1c5f6260153e
Frieza01 <blyumenshtejn2@gmail.com>
2024-12-15 15:14:02 +0300

    Добавьте файлы проекта.

---
+++ Training/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Даниил"; 
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
